4f6f7d4488c4ea1adce2eea4b131cd1eb822545f
jaywcjlove <398188662@qq.com>
2021-02-17 00:24:20 +0800

    website: update example.

--- example/app/App.tsx
+++ example/app/App.tsx
@@ -1,8 +1,8 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { editor } from 'monaco-editor';
 // @ts-ignore
 // eslint-disable-next-line import/no-unresolved, import/extensions
-import MonacoEditor from '@uiw/react-monacoeditor';
+import MonacoEditor, { RefEditorInstance } from '../../';
 import Markdown from '@uiw/react-markdown-preview';
 import Select from './Select';
 import logo from './logo.svg';
@@ -21,129 +21,125 @@ export const languageData = [
   'turtle', 'twig', 'typescript', 'vb', 'vbscript', 'velocity', 'verilog', 'vhdl', 'vue', 'webidl', 'xml', 'xquery', 'yacas', 'yaml',
 ];
 
-interface AppProps {}
-interface AppState {
-  code: string;
-  mode: string;
-  hyperlink: Record<'href' | 'label', string>[];
-}
+const themesData = ['vs', 'vs-dark', 'hc-black']
 
-class App extends PureComponent<AppProps, AppState> {
-  editor?: editor.IStandaloneCodeEditor;
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      code: '',
-      mode: 'javascript',
-      hyperlink: [
-        {
-          href: 'https://github.com/jaywcjlove/react-monacoeditor',
-          label: 'View on GitHub',
-        },
-        {
-          href: 'https://www.npmjs.com/package/react-monacoeditor',
-          label: 'View on NPM',
-        },
-        {
-          href: 'https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditoroptions.html#linenumbers',
-          label: 'Monaco Editor Options',
-        },
-      ],
-    };
-  }
-  componentDidMount() {
-    this.dynamicLoadable(this.state.mode).then((code) => {
-      this.setState({ mode: this.state.mode, code: code.default || '' });
-    });
-  }
-  editorDidMount(editor: editor.IStandaloneCodeEditor) {
+export default function Example() {
+  const [theme, setTheme] = useState('vs-dark');
+  const [code, setCode] = useState('');
+  const [mode, setMode] = useState('javascript');
+  const [hyperlink, setHyperlink] = useState<Record<'href' | 'label', string>[]>([
+    {
+      href: 'https://github.com/jaywcjlove/react-monacoeditor',
+      label: 'View on GitHub',
+    },
+    {
+      href: 'https://www.npmjs.com/package/react-monacoeditor',
+      label: 'View on NPM',
+    },
+    {
+      href: 'https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditoroptions.html#linenumbers',
+      label: 'Monaco Editor Options',
+    },
+  ]);
+  const editorRef = useRef<RefEditorInstance>(null);
+  // @ts-ignore
+  const version = VERSION; // eslint-disable-line
+  const options: editor.IStandaloneEditorConstructionOptions & editor.IEditorScrollbarOptions = {
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    cursorStyle: 'line',
+    automaticLayout: false,
+    theme,
+    // // Subtle shadows to the left & top. Defaults to true.
+    // useShadows: false,
+    // // Render vertical arrows. Defaults to false.
+    // verticalHasArrows: true,
+    // // Render horizontal arrows. Defaults to false.
+    // horizontalHasArrows: true,
+    // // Render vertical scrollbar.
+    // // Accepted values: 'auto', 'visible', 'hidden'.
+    // // Defaults to 'auto'
+    // vertical: 'visible',
+    // // Render horizontal scrollbar.
+    // // Accepted values: 'auto', 'visible', 'hidden'.
+    // // Defaults to 'auto'
+    // horizontal: 'visible',
+    // verticalScrollbarSize: 17,
+    // horizontalScrollbarSize: 17,
+    // arrowSize: 30,
+  };
+  let DocumentStrSource = DocumentStr;
+  if (DocumentStrSource) DocumentStrSource = DocumentStr.replace(/([\s\S]*)<!--dividing-->/, '');
+
+  function editorDidMount(editor: editor.IStandaloneCodeEditor) {
     // console.log('editorDidMount', editor, monaco); // eslint-disable-line
     editor.focus();
   }
-  onChange() {
-    // console.log('onChange', newValue, e);  // eslint-disable-line
-  }
-  onSelectChange(e: React.ChangeEvent<HTMLSelectElement>) {
+  function onChange() {}
+  function onSelectChange(e: React.ChangeEvent<HTMLSelectElement>) {
     e.persist();
     const lang = e.target.value;
     if (languageData.indexOf(lang) === -1) {
-      this.setState({ mode: lang });
+      setMode(lang);
       return;
     }
-    this.dynamicLoadable(lang).then((code) => {
-      // console.log('code.default:', code.default);
-      this.setState({ mode: lang, code: code.default || '' });
+    // editorRef.current?.editor.set
+    const model = editorRef.current?.editor?.getModel()
+    if (model) {
+      editor.setModelLanguage(model, lang || '');
+    }
+    setMode(lang);
+    dynamicLoadable(lang).then((code) => {
+      setCode(code.default || '');
     });
   }
-  dynamicLoadable(lang: string) {
+  function onSelectThemeChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    e.persist();
+    setTheme(e.target.value);
+  }
+  function dynamicLoadable(lang: string) {
     return import(`code-example/lib/${lang}.js`);
   }
-  render(): JSX.Element {
-    const { hyperlink, code } = this.state;
-    // @ts-ignore
-    const version = VERSION; // eslint-disable-line
-    const options = {
-      selectOnLineNumbers: true,
-      roundedSelection: false,
-      cursorStyle: 'line',
-      automaticLayout: false,
-      theme: 'vs-dark',
-    };
-    let DocumentStrSource = DocumentStr;
-    if (DocumentStrSource) DocumentStrSource = DocumentStr.replace(/([\s\S]*)<!--dividing-->/, '');
-    return (
-      <div className={styles.App}>
-        <header className={styles.AppHeader}>
-          <img src={logo} className={styles.AppLogo} alt="logo" />
-          <h1 className={styles.AppTitle}>React-MonacoEditor <sup>v{version}</sup></h1>
-          <p className={styles.content}>MonacoEditor component for React. </p>
-          <div className={styles.button}>
-            {hyperlink.map((item, idx) => {
-              return (
-                <a key={idx} target="_blank" rel="noopener noreferrer" href={item.href}>{item.label}</a>
-              );
-            })}
-          </div>
-        </header>
-        <div className={styles.editor}>
-          <MonacoEditor
-            ref={(editor: editor.IStandaloneCodeEditor) => this.editor = editor}
-            height="500px"
-            language={this.state.mode}
-            value={code}
-            editorDidMount={this.editorDidMount.bind(this)}
-            onChange={this.onChange.bind(this)}
-            options={options}
-            scrollbar={{
-              // Subtle shadows to the left & top. Defaults to true.
-              useShadows: false,
-              // Render vertical arrows. Defaults to false.
-              verticalHasArrows: true,
-              // Render horizontal arrows. Defaults to false.
-              horizontalHasArrows: true,
-              // Render vertical scrollbar.
-              // Accepted values: 'auto', 'visible', 'hidden'.
-              // Defaults to 'auto'
-              vertical: 'visible',
-              // Render horizontal scrollbar.
-              // Accepted values: 'auto', 'visible', 'hidden'.
-              // Defaults to 'auto'
-              horizontal: 'visible',
-              verticalScrollbarSize: 17,
-              horizontalScrollbarSize: 17,
-              arrowSize: 30,
-            }}
-          />
-        </div>
-        <div className={styles.options}>
-          <Select value={this.state.mode} options={languageData} onChange={this.onSelectChange.bind(this)} />
-        </div>
-        <div>
-        <Markdown source={DocumentStrSource} className={styles.markdown} />
-        </div>
-      </div>
-    );
-  }
-}
 
-export default App;
+  useEffect(() => {
+    dynamicLoadable(mode).then((code) => {
+      setCode(code.default || '');
+      // setMode(mode);
+      // this.setState({ mode: this.state.mode, code: code.default || '' });
+    });
+  }, []);
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
+        <h1 className={styles.AppTitle}>React-MonacoEditor <sup>v{version}</sup></h1>
+        <p className={styles.content}>MonacoEditor component for React. </p>
+        <div className={styles.button}>
+          {hyperlink.map((item, idx) => {
+            return (
+              <a key={idx} target="_blank" rel="noopener noreferrer" href={item.href}>{item.label}</a>
+            );
+          })}
+        </div>
+      </header>
+      <div className={styles.editor}>
+        <MonacoEditor
+          ref={editorRef}
+          height="500px"
+          language={mode}
+          value={code}
+          editorDidMount={(editor) => editorDidMount(editor)}
+          onChange={onChange}
+          options={options}
+        />
+      </div>
+      <div className={styles.options}>
+        <Select value={mode} options={languageData} onChange={onSelectChange} />
+        <Select value={theme} options={themesData} onChange={onSelectThemeChange} />
+      </div>
+      <div>
+      <Markdown source={DocumentStrSource} className={styles.markdown} />
+      </div>
+    </div>
+  )
+}
