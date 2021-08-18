c0d0784162a656ad6668f0200217b374c779724f
jaywcjlove <jaywcjlove@users.noreply.github.com>
2021-08-18 14:07:02 +0000

    deploy: 29da77c405784a363cf0e2475294a9c90a93c3fc

---
+++ static/js/134.b46ecf50.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uiw/react-monacoeditor"]=this["webpackJsonp@uiw/react-monacoeditor"]||[]).push([[134],{1138:function(e,n,i){"use strict";i.r(n),n.default='begin:\nTextWindow.Write("Enter a number: ")\nnum = TextWindow.ReadNumber()\nremainder = Math.Remainder(num, 2)\nIf (remainder = 0) Then\n TextWindow.WriteLine("The number is Even")\nElse\n TextWindow.WriteLine("The number is Odd")\nEndIf\nGoto begin'}}]);
+//# sourceMappingURL=134.b46ecf50.chunk.js.map
