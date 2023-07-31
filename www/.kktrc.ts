bbb4c2444afaae926d0f4137b859de47580bc805
jaywcjlove <398188662@qq.com>
2023-07-31 09:29:24 +0800

    website(deps): Modify kkt config.

--- www/.kktrc.ts
+++ www/.kktrc.ts
@@ -1,24 +1,15 @@
-import path from 'path';
 import webpack, { Configuration } from 'webpack';
 import { LoaderConfOptions } from 'kkt';
-import lessModules from '@kkt/less-modules';
 import rawModules from '@kkt/raw-modules';
-import scopePluginOptions from '@kkt/scope-plugin-options';
+import { disableScopePlugin } from '@kkt/scope-plugin-options';
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 import { mdCodeModulesLoader } from 'markdown-react-code-preview-loader';
 import pkg from './package.json';
 
 export default (conf: Configuration, env: 'production' | 'development', options: LoaderConfOptions) => {
-  conf = lessModules(conf, env, options);
   conf = rawModules(conf, env, { ...options, test: /\.(md|txt)$/i });
   conf = mdCodeModulesLoader(conf);
-  conf = scopePluginOptions(conf, env, {
-    ...options,
-    allowedFiles: [
-      path.resolve(process.cwd(), 'README.md'),
-      path.resolve(process.cwd(), 'src'),
-    ]
-  });
+  conf = disableScopePlugin(conf);
   conf.plugins!.push(new MonacoWebpackPlugin({
     // languages: ['bat', 'coffee', 'cpp', 'csharp', 'csp', 'css', 'dockerfile', 'fsharp', 'go', 'handlebars', 'html', 'ini', 'java', 'json', 'less', 'lua', 'markdown', 'msdax', 'mysql', 'objective', 'pgsql', 'php', 'postiats', 'powershell', 'pug', 'python', 'r', 'razor', 'redis', 'redshift', 'ruby', 'sb', 'scss', 'solidity', 'sql', 'swift', 'typescript', 'vb', 'xml', 'yaml'],
     // features: ['accessibilityHelp', 'bracketMatching', 'caretOperations', 'clipboard', 'codelens', 'colorDetector', 'comment', 'contextmenu', 'coreCommands', 'cursorUndo', 'dnd', 'find', 'folding', 'format', 'gotoDeclarationCommands', 'gotoDeclarationMouse', 'gotoError', 'gotoLine', 'hover', 'inPlaceReplace', 'inspectTokens', 'iPadShowKeyboard', 'linesOperations', 'links', 'multicursor', 'parameterHints', 'quickCommand', 'quickFixCommands', 'quickOutline', 'referenceSearch', 'rename', 'smartSelect', 'snippets', 'suggest', 'toggleHighContrast', 'toggleTabFocusMode', 'transpose', 'wordHighlighter', 'wordOperations'],
